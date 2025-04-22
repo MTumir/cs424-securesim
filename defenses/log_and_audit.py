b708c746cdce3ff2576398fd9efe115b9946df6d
Max Tumir <98791648+MTumir@users.noreply.github.com>
2025-04-22 16:48:48 -0600

    Added comments to Injection and Logging (#23)

--- defenses/log_and_audit.py
+++ defenses/log_and_audit.py
@@ -1,18 +1,21 @@
 import logging
+logger = logging.getLogger(__name__)
 
 class LogAndAudit:
     def __init__(self, tc):
         self.active = False
         self.tc = tc
-        self.log = logging.getLogger(__name__)
         self.handler = None
 
     def activate(self):
+        # Create a new stream for the log file
         self.handler = logging.FileHandler('data/temperature_control.log', mode='w')
+        # Assign handler the same format as defined in main.py
         self.handler.setFormatter(logging.getLogger().handlers[0].formatter)
+        # Add handler to the logger
         logging.getLogger().addHandler(self.handler)
 
-        self.log.info('Logging defense activated')
+        logger.info('Logging defense activated')
         self.active = True
 
     def deactivate(self):
